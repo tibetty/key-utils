7be9bf91ee9aad6c52a710e19107b694446a7430
git <xihua.duan@tourank.com>
2018-12-18 16:46:17 +0800

    refactoring

--- key-utils.js
+++ key-utils.js
@@ -7,7 +7,7 @@ const keyUtils = (() => {
 		}
 	}
 	
-	async function ku$ConvertKeyPair(keypair, extractable, srcAlgoName, dstAlgoName, pkOps, skOps) {
+	async function ku$convertKeyPair(keypair, extractable, srcAlgo, dstAlgo, pkOps, skOps) {
 		const errMessages = [
 			'Not a valid key pair',
 			'Key pair is not extractable',
@@ -16,20 +16,20 @@ const keyUtils = (() => {
 
 		let pk = keypair.publicKey, sk = keypair.privateKey;
 		ku$assert(pk !== null && sk !== null, errMessages[0]);
-		ku$assert(pk.algorithm !== null && pk.algorithm.name === srcAlgoName && sk.algorithm !== null && sk.algorithm.name === srcAlgoName, errMessages[0]);
+		ku$assert(pk.algorithm !== null && pk.algorithm.name === srcAlgo && sk.algorithm !== null && sk.algorithm.name === srcAlgo, errMessages[0]);
 		ku$assert(pk.extractable && sk.extractable, errMessages[1]);
 		ku$assert(crypto !== null && crypto.subtle !== null, errMessages[2]);
 		const subtle = crypto.subtle;
 		extractable = extractable || false;
 		let publicKey = await subtle.importKey('raw', await subtle.exportKey('raw', pk), {
-				name: dstAlgoName,
+				name: dstAlgo,
 				namedCurve: pk.algorithm.namedCurve
 			},
 			extractable,
 			pkOps
 		);
 		let privateKey = await subtle.importKey('jwk', await Object.assign(await subtle.exportKey('jwk', sk), {key_ops: skOps}), {
-				name: dstAlgoName,
+				name: dstAlgo,
 				namedCurve: sk.algorithm.namedCurve
 			},
 			extractable,
@@ -39,7 +39,7 @@ const keyUtils = (() => {
 	}
 
 	return {
-		ecdhToEcdsa: async (keypair, extractable) => await ku$ConvertKeyPair(keypair, extractable, 'ECDH', 'ECDSA', ['verify'], ['sign']),
-		ecdsaToEcdh: async (keypair, extractable) => await ku$ConvertKeyPair(keypair, extractable, 'ECDSA', 'ECDH', [], ['deriveBits', 'deriveKey'])
+		ecdhToEcdsa: async (keypair, extractable) => await ku$convertKeyPair(keypair, extractable, 'ECDH', 'ECDSA', ['verify'], ['sign']),
+		ecdsaToEcdh: async (keypair, extractable) => await ku$convertKeyPair(keypair, extractable, 'ECDSA', 'ECDH', [], ['deriveBits', 'deriveKey'])
 	};
 })();
